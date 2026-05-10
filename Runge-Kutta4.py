badaae764531820d2a82690071150c6f1c41840c
Bebra_02 <savvabaryshev@gmail.com>
2026-05-10 18:17:29 +0300

    def Runge_Kutta4 description added

--- Runge-Kutta4.py
+++ Runge-Kutta4.py
@@ -1,5 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
 from math import *
 
 
 def Runge_Kutta4(function, innit_condition_x, innit_condition_y, end_condition_x, time_step):
+    # функция, реализующая метод численного интегрирования системы обыкновенных дифференциальных уравнений, метод Рунге-Кутты 4-ого порядка
+    # function - функция математической модели, принимаемая на вход алгоритмом Runge_Kutta4
+    # innit_condition_x - начальные условия независимой переменной
+    # innit_condition_y - начальные условия зависимой переменной
+    # end_condition_x - конечное условие для независимой переменной
+    # time_step - шаг константы интегрирования (в нашем случае, шаг по времени)
+
     return
